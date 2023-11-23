03bf85e17879aa73f87f955cf972cebde1a7b41e
Ayulestari1756 <lestariia563@gmail.com>
2023-11-24 01:05:17 +0700

    commit message

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', 'HomeController@index');
+
+
+Route::get('/about', function () {
+    $data = [
+        'pageTitle' => 'Tentang Kami',
+        'content' => 'Ini adalah halaman tentang kami.'
+    ];
+    return view('about', $data);
+});
+
+Route::get('/produk', 'App\Http\Controllers\ProdukController@index');
+
+Route::resource('produk', ProdukController::class, ['except' => [
+    'create', 'update', 'delete', 'show'
+
+]]);
